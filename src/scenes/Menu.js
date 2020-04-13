f3054d00b5553e3be775f35722fe7551c84fb356
Parker Soto <pdsoto@ucsc.edu>
2020-04-13 16:35:26 -0700

    Added Assets and Updated playScene

---
+++ src/scenes/Menu.js
@@ -0,0 +1,13 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene")
+    }
+
+    create() {
+        //Display menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        //Launches next scene
+        this.scene.start("playScene");
+    }
+}
